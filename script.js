825e0d1d16668491e09629d2cfc7de56678f3549
Shefali <mittal.shefali2000@gmail.com>
2025-01-11 13:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+// script.js
+function changeImage() {
+    const img = document.querySelector('.animal-image');
+    img.src = 'https://example.com/another-elephant.jpg'; // Change to another image URL
+}
+
+// Add an event listener to change the image on click
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.querySelector('.animal-image');
+    img.addEventListener('click', changeImage);
+});
